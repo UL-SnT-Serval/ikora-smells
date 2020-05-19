3908bd172f24e6fc7201a7432134af2ecffaf7ac
kabinja <renaudrwemalika@gmail.com>
2020-05-19 16:51:45 +0200

    update missing assertion check

--- src/main/java/tech/ikora/smells/checks/MissingAssertionCheck.java
+++ src/main/java/tech/ikora/smells/checks/MissingAssertionCheck.java
@@ -1,6 +1,7 @@
 package tech.ikora.smells.checks;
 
 import tech.ikora.analytics.visitor.PathMemory;
+import tech.ikora.model.Step;
 import tech.ikora.model.TestCase;
 import tech.ikora.smells.SmellCheck;
 import tech.ikora.smells.SmellMetric;
@@ -9,9 +10,20 @@ import tech.ikora.smells.visitors.MissingAssertionVisitor;
 public class MissingAssertionCheck implements SmellCheck {
     @Override
     public SmellMetric computeMetric(TestCase testCase) {
-        MissingAssertionVisitor visitor = new MissingAssertionVisitor();
-        visitor.visit(testCase, new PathMemory());
 
-        return new SmellMetric(SmellMetric.Type.MISSING_ASSERTION, visitor.getAssertionCount());
+        int missingAssertionSteps = 0;
+
+        for(Step step: testCase.getSteps()){
+            MissingAssertionVisitor visitor = new MissingAssertionVisitor();
+            visitor.visit(step, new PathMemory());
+
+            if(visitor.getAssertionCount() == 0){
+                ++missingAssertionSteps;
+            }
+        }
+
+        double metric = (double)missingAssertionSteps / (double)testCase.getSteps().size();
+
+        return new SmellMetric(SmellMetric.Type.MISSING_ASSERTION, metric);
     }
 }
