f74c00e79e4758ab733e477f16e861d50dc05518
kabinja <renaudrwemakika@gmail.com>
2020-07-07 14:47:52 +0200

    implement using personal pronoun

--- src/main/java/tech/ikora/smells/checks/UsingPersonalPronounCheck.java
+++ src/main/java/tech/ikora/smells/checks/UsingPersonalPronounCheck.java
@@ -1,16 +1,62 @@
 package tech.ikora.smells.checks;
 
-import org.apache.commons.lang3.NotImplementedException;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import tech.ikora.model.Step;
 import tech.ikora.model.TestCase;
 import tech.ikora.smells.SmellCheck;
 import tech.ikora.smells.SmellDetector;
 import tech.ikora.smells.SmellMetric;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
 public class UsingPersonalPronounCheck implements SmellCheck {
     @Override
     public SmellMetric computeMetric(TestCase testCase, SmellDetector detector) {
+        long personalPronoun = calculateUsingPersonalPronoun(testCase.getSteps());
+        double metric = (double)personalPronoun / (double)testCase.getSteps().size();
 
+        return new SmellMetric(SmellMetric.Type.USING_PERSONAL_PRONOUN, metric);
+    }
 
-        throw new NotImplementedException("Check is not implemented yet: " + this.getClass().getName());
+    private long calculateUsingPersonalPronoun(List<Step> steps){
+        return steps.stream().filter(this::isUsingPersonalPronoun).count();
+    }
+
+    private boolean isUsingPersonalPronoun(Step step){
+        final String text = step.getName();
+
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos");
+        props.setProperty("coref.algorithm", "neural");
+
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        CoreDocument document = new CoreDocument(text);
+        pipeline.annotate(document);
+
+        for(CoreSentence sentence: document.sentences()){
+            if(findPronoun(sentence).isPresent()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Optional<String> findPronoun(CoreSentence sentence){
+        for(int i = 0; i < sentence.tokens().size(); ++i){
+            if(sentence.posTags().get(i).startsWith("VB")){
+                break;
+            }
+
+            if(sentence.posTags().get(i).equals("PRP")){
+                return Optional.of(sentence.tokens().get(i).value());
+            }
+        }
+
+        return Optional.empty();
     }
 }
---
+++ src/test/java/tech/ikora/smells/UsingPersonalPronounCheckTest.java
@@ -0,0 +1,36 @@
+package tech.ikora.smells;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import tech.ikora.model.Project;
+import tech.ikora.model.TestCase;
+import tech.ikora.smells.checks.UsingPersonalPronounCheck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsingPersonalPronounCheckTest {
+    private static Project project;
+
+    @BeforeAll
+    static void loadProject(){
+        project = Helpers.compileProject("usingPersonalPronoun.robot");
+    }
+
+    @Test
+    void testWithNoPronoun(){
+        final TestCase testCase = project.findTestCase("usingPersonalPronoun", "Test case without personal pronoun").iterator().next();
+        final UsingPersonalPronounCheck check = new UsingPersonalPronounCheck();
+        final SmellMetric metric = check.computeMetric(testCase, null);
+
+        assertEquals(metric.getValue(), 0.0);
+    }
+
+    @Test
+    void testWithAllPronoun(){
+        final TestCase testCase = project.findTestCase("usingPersonalPronoun", "Test case with all personal pronoun").iterator().next();
+        final UsingPersonalPronounCheck check = new UsingPersonalPronounCheck();
+        final SmellMetric metric = check.computeMetric(testCase, null);
+
+        assertEquals(metric.getValue(), 1.0);
+    }
+}
