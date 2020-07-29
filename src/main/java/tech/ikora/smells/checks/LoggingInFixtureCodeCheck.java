05d22076ece6bfe0f58809307f2beef48635fa23
kabinja <renaudrwemakika@gmail.com>
2020-07-29 17:37:38 +0200

    if there is no setup, LoggingInFixtureCodeCheck should return NaN and add tests for LoggingInFixtureCodeCheck

--- src/main/java/tech/ikora/smells/checks/LoggingInFixtureCodeCheck.java
+++ src/main/java/tech/ikora/smells/checks/LoggingInFixtureCodeCheck.java
@@ -13,17 +13,22 @@ import tech.ikora.smells.SmellMetric;
 import tech.ikora.smells.SmellResult;
 import tech.ikora.smells.visitors.CollectCallsByTypeVisitor;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class LoggingInFixtureCodeCheck implements SmellCheck {
     @Override
     public SmellResult computeMetric(TestCase testCase, SmellDetector detector) {
-        int statements = testCase.getSetup().map(KeywordStatistics::getStatementCount).orElse(0)
-                + testCase.getTearDown().map(KeywordStatistics::getStatementCount).orElse(0);
+        Set<SourceNode> nodes = Collections.emptySet();
+        double metric = Double.NaN;
 
-        Set<SourceNode> nodes = getFixtureLoggingNodes(testCase);
+        if(testCase.getSetup().isPresent() || testCase.getTearDown().isPresent()){
+            int statements = testCase.getSetup().map(KeywordStatistics::getStatementCount).orElse(1) - 1
+                    + testCase.getTearDown().map(KeywordStatistics::getStatementCount).orElse(1) - 1;
 
-        double metric = (double)nodes.size() / (double)statements;
+            nodes = getFixtureLoggingNodes(testCase);
+            metric = (double)nodes.size() / (double)statements;
+        }
 
         return new SmellResult(SmellMetric.Type.LOGGING_IN_FIXTURE_CODE, metric, nodes);
     }
