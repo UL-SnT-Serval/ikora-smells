af80025ea98ae6dc7143dc722a56595ef5a3956a
kabinja <renaudrwemalika@gmail.com>
2020-08-04 14:52:38 +0200

    add json parser annotation for smell configuration

--- src/main/java/tech/ikora/smells/SmellConfiguration.java
+++ src/main/java/tech/ikora/smells/SmellConfiguration.java
@@ -1,11 +1,25 @@
 package tech.ikora.smells;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import tech.ikora.analytics.clones.Clones;
 import tech.ikora.model.UserKeyword;
 
 public class SmellConfiguration {
-    private Clones<UserKeyword> clones = new Clones<>();
+    @JsonProperty(value = "maximum step size")
     private int maximumStepSize = 30;
+    @JsonIgnore
+    private Clones<UserKeyword> clones = new Clones<>();
+
+    @JsonProperty(value = "maximum step size")
+    public int getMaximumStepSize() {
+        return maximumStepSize;
+    }
+
+    @JsonProperty(value = "maximum step size")
+    public void setMaximumStepSize(int maximumStepSize) {
+        this.maximumStepSize = maximumStepSize;
+    }
 
     public void setClones(Clones<UserKeyword> clones){
         this.clones = clones;
@@ -13,13 +27,5 @@ public class SmellConfiguration {
 
     public Clones<UserKeyword> getClones(){
         return this.clones;
-    }
-
-    public int getMaximumStepSize() {
-        return maximumStepSize;
-    }
-
-    public void setMaximumStepSize(int maximumStepSize) {
-        this.maximumStepSize = maximumStepSize;
     }
 }
