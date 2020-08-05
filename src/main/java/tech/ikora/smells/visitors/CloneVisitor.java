2dc6e2b8bb9d63434fd55738916e575560165439
kabinja <renaudrwemakika@gmail.com>
2020-08-05 14:06:56 +0200

    adapt to new version of the clone detection

--- src/main/java/tech/ikora/smells/visitors/CloneVisitor.java
+++ src/main/java/tech/ikora/smells/visitors/CloneVisitor.java
@@ -1,6 +1,5 @@
 package tech.ikora.smells.visitors;
 
-import tech.ikora.analytics.clones.Clone;
 import tech.ikora.analytics.clones.Clones;
 import tech.ikora.analytics.visitor.VisitorMemory;
 import tech.ikora.model.KeywordDefinition;
@@ -25,7 +24,7 @@ public class CloneVisitor extends SmellVisitor {
 
     @Override
     public void visit(UserKeyword keyword, VisitorMemory memory) {
-        if(this.clones.getCloneType(keyword) != Clone.Type.NONE){
+        if(this.clones.getCloneType(keyword) != Clones.Type.NONE){
             addNode(keyword);
         }
         ++totalKeywordsCounter;
